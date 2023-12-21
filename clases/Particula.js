37455963706422b32a46bc58d748dd3539ec121e
Johany Bravo <mayli.bravo@ucuenca.edu.ec>
2023-12-20 23:00:42 -0500

    Se modificó la forma y de cambió la posición

--- clases/Particula.js
+++ clases/Particula.js
@@ -1,37 +1,37 @@
 class Particula {
-	constructor(_x, _y, _color) {
-		this.pos = createVector(_x, _y);
-		this.lifespan = Math.ceil(random(10, 250));
-		this.isAlive = true;
-		this.side = Math.ceil(random(16, 17));
-		this.color =(random(255, 183, 3), 285);
-	}
-	update() {
-		this.r = random(50);
+  constructor(_x, _y, _color) {
+    this.pos = createVector(_x, _y);
+    this.lifespan = Math.ceil(random(10, 250));
+    this.isAlive = true;
+    this.side = Math.ceil(random(16, 17));
+    this.color = (255, 183, 3, 285);
+  }
+  update() {
+    this.r = random(50);
 
-		if (this.r < 0) {
-			// hacia arriba
-			this.pos.y -= this.side;
-		} else if (this.r < 100) {
-			// hacia la derecha
-			this.pos.x += this.side;
-		} else if (this.r < 1) {
-			// hacia abajo
-			this.pos.y += this.side;
-		} else {
-			// hacia izquierda
-			this.pos.x -= this.side;
-		}
+    if (this.r < 0) {
+      // hacia arriba
+      this.pos.y -= this.side;
+    } else if (this.r < 50) {
+      // hacia la derecha
+      this.pos.x += this.side;
+    } else if (this.r < 1) {
+      // hacia abajo
+      this.pos.y += this.side;
+    } else {
+      // hacia izquierda
+      this.pos.x -= this.side;
+    }
 
-		this.lifespan -= 0;
+    this.lifespan -= 0;
 
-		if (this.lifespan <= 0) {
-			this.isAlive = false;
-		}
-	}
-	display() {
-		fill(this.color);
-		stroke(255);
-		circle(this.pos.x, this.pos.y, this.side);
-	}
+    if (this.lifespan <= 0) {
+      this.isAlive = false;
+    }
+  }
+  display() {
+    fill(color(random(20), random(250, 25), 200));
+    stroke(0, 170, 200);
+    circle(this.pos.x, this.pos.y, this.side);
+  }
 }
--- sketch.js
+++ sketch.js
@@ -4,13 +4,17 @@ let t;
 let s2;
 let v2;
 let t2;
+let v3;
+let t3;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  s = new Sistema ();
-  v = new Sistema ();
-  s2 = new Sistema ();
-  v2 = new Sistema ();
+  s = new Sistema();
+  v = new Sistema();
+  s2 = new Sistema();
+  v2 = new Sistema();
+  s3 = new Sistema();
+  v3 = new Sistema();
 }
 
 function draw() {
@@ -23,4 +27,6 @@ function draw() {
   s2.display();
   v2.update();
   v2.display();
+  v3.update();
+  v3.display();
 }
